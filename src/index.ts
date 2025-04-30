bc4581945089db8be24aff5191d8fc3e1759e58e
Amogh S Thampuran <amoghsthampuran@gmail.com>
2025-04-30 08:58:30 +0530

    feature/initial server setup

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+
 require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middleware
+app.use(bodyParser.json());
+
+// Routes
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
