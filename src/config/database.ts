fc8475f13d2eaabae13bb402cc6c709675c9090f
Amogh S Thampuran <amoghsthampuran@gmail.com>
2025-05-01 12:18:26 +0530

    fix/sequelize connect

--- src/config/database.ts
+++ src/config/database.ts
@@ -3,14 +3,17 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const sequelize = new Sequelize({
-  dialect: 'mysql', // or 'postgres', 'sqlite', etc.
-  host: process.env.DB_HOST,
-  port: Number(process.env.DB_PORT),
-  username: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  logging: false,
-});
+const sequelize = new Sequelize(process.env.DB_NAME!, process.env.DB_USER!, process.env.DB_PASSWORD!, {
+    host: process.env.DB_HOST,               // e.g. 'localhost'
+    port: Number(process.env.DB_PORT),                  // now fixed
+    dialect: 'mssql',
+    dialectOptions: {
+      options: {
+        encrypt: false,
+        trustServerCertificate: true
+      }
+    },
+    logging: false
+  });
 
 export default sequelize;
