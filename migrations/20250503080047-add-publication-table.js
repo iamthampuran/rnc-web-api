9c4052dbf2e4cd69f09eb8e980fd46bef881e542
Amogh S Thampuran <amoghsthampuran@gmail.com>
2025-05-03 13:58:40 +0530

    feature/migrations and setup enums for publications

---
+++ migrations/20250503080047-add-publication-table.js
@@ -0,0 +1,101 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+    await queryInterface.createTable('PublicationStatus', {
+      id: {type: Sequelize.INTEGER, autoIncrement: true, primaryKey: true},
+      name: {type: Sequelize.STRING, allowNull: false},
+    });
+
+    await queryInterface.createTable('Publications', {
+      id: {type: Sequelize.INTEGER, autoIncrement: true, primaryKey: true},
+      academicYear: {type: Sequelize.STRING, allowNull: false},
+      title: {type: Sequelize.STRING, allowNull: false},
+      affiliated: {type: Sequelize.BOOLEAN, allowNull: false},
+      name: {type: Sequelize.STRING, allowNull: false},
+      details: {type: Sequelize.STRING, allowNull: false},
+      impactFactor: {type: Sequelize.STRING, allowNull: false},
+      typeId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'Type',
+          key: 'id'
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE'
+      },
+      subtypeId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'SubType',
+          key: 'id'
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE'
+      },
+      statusId:{
+        type: Sequelize.INTEGER,
+        allowNull:false,
+        references:{
+          model:'PublicationStatus',
+          key:'id'
+        },
+        onUpdate:'CASCADE',
+        onDelete:'CASCADE'
+      },
+      createdAt:{type:Sequelize.DATE, defaultValue:new Date()},
+      updatedAt:{type:Sequelize.DATE, defaultValue:new Date()}
+    });
+
+    await queryInterface.createTable('UserPublications', {
+      userId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'Users',
+          key: 'id'
+        },
+        onDelete: 'CASCADE',
+        onUpdate: 'CASCADE'
+      },
+      publicationId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'Publications',
+          key: 'id'
+        },
+        onDelete: 'CASCADE',
+        onUpdate: 'CASCADE'
+      }
+    });
+
+    await queryInterface.bulkInsert('PublicationStatus', [
+      {name: 'Requested'},
+      {name: 'Approved'},
+      {name: 'Rejected'}
+    ], {});
+
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+    await queryInterface.dropTable('UserPublications');
+    await queryInterface.dropTable('Publications');
+    await queryInterface.dropTable('PublicationStatus');
+  }
+};
